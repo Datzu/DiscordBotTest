35ab1ef4678c5fa32023712989670d76e662515f
Datzu <murkhies@gmail.com>
2019-09-14 01:07:07 +0200

    Secure the bot key

--- DiscordBotTest/src/main/java/com/discordbotapp/main/Main.java
+++ DiscordBotTest/src/main/java/com/discordbotapp/main/Main.java
@@ -1,5 +1,9 @@
 package com.discordbotapp.main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.AccountType;
@@ -11,8 +15,25 @@ public class Main extends ListenerAdapter {
 
 	public static void main(String[] args) {
 		JDABuilder builder = new JDABuilder(AccountType.BOT);
-		String token = "NjIxMDM2ODI1MDc0NTk3OTEw.XXwUUw.x-OdMOW5EgzpjQBFttlbhJEvONI";
-		builder.setToken(token);
+		
+		InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties");
+
+        Properties prop = new Properties();
+
+        if (input == null) {
+            System.out.println("Sorry, unable to find config.properties");
+            return;
+        }
+        
+        try {
+			prop.load(input);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+        System.out.println(prop.getProperty("bot.token"));
+		
+		builder.setToken(prop.getProperty("bot.token"));
 		builder.addEventListeners(new Main());
 		try {
 			builder.build();
